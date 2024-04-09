a929f86e20dfc77f163949bbd1797a09f28c9d79
GediminasGsn <gasiunasgediminas@gmail.com>
2024-04-09 15:11:39 +0300

    **

--- src/test/java/smokeTest.java
+++ src/test/java/smokeTest.java
@@ -136,9 +136,9 @@ public class smokeTest {
 
             // Add additional waiting time if needed after navigating back
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//            WebElement element = driver.findElement(By.id("element-id"));
-//            WebDriverWait waiter = new WebDriverWait(driver, Duration.ofSeconds(40));
-//            waiter.until(ExpectedConditions.invisibilityOf(element));
+            WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[4]/div[6]/div"));
+            WebDriverWait waiter = new WebDriverWait(driver, Duration.ofSeconds(40));
+            waiter.until(ExpectedConditions.invisibilityOf(element));
         }
 
     }
